e7bec30b8f3444e4550643b08be66abb57a9c7b9
mitsuji <mitsuji@na-s.jp>
2025-06-27 12:50:01 +0900

    mod: 数値のフォーマットと小計・合計

--- web/polyform.js
+++ web/polyform.js
@@ -1,3 +1,6 @@
+const fromFormattedNumber = (txt) => Number(txt.replace(",",""));
+const toFormattedNumber = (num) => num.toLocaleString('ja-JP');
+
 window.addEventListener('load',(event) => {
     const { createApp, ref, onMounted, watch } = Vue;
     const app = createApp({
@@ -8,11 +11,32 @@ window.addEventListener('load',(event) => {
                 // [TODO] フォームに入力した時点でエラーや警告を出したいときはここを使う
                 console.log ("form changed...");
             },{deep : true});
+            const calcSum = () => {
+                let quantityTotal = 0;
+                let sumTotal = 0;
+                for(let i = 0; i < 3; i++) {
+                    const item = form.value[`item${i}`];
+                    const blankPrice = (item.price == "");
+                    const blankQuantity = (item.quantity == "");
+                    const price = (blankPrice) ? NaN : fromFormattedNumber(item.price);
+                    const quantity = (blankQuantity) ? NaN : Math.trunc(fromFormattedNumber(item.quantity));
+                    const sum = price * quantity;
+                    item.price = isNaN(price) ? "" : toFormattedNumber(price);
+                    item.quantity = isNaN(quantity) ? "" : toFormattedNumber(quantity);
+                    item.sum = isNaN(sum) ? "" : toFormattedNumber(sum);
+                    quantityTotal += (blankPrice && blankQuantity) ? 0 : quantity;
+                    sumTotal +=  (blankPrice && blankQuantity) ? 0 : sum;
+                }
+                form.value.quantityTotal = (isNaN(quantityTotal) || quantityTotal == 0)
+                                            ? "" : toFormattedNumber(quantityTotal);
+                form.value.sumTotal = (isNaN(sumTotal) || quantityTotal == 0)
+                                        ? "" : toFormattedNumber(sumTotal);
+            };
             const appendForm = (index) => {
                 forms.value.push ({code:'',name:'',age:'',isActive:false,
-                                   item0:{code:'',name:'',price:''},
-                                   item1:{code:'',name:'',price:''},
-                                   item2:{code:'',name:'',price:''}
+                                   item0:{code:'',name:'',price:'',quantity:'',sum:''},
+                                   item1:{code:'',name:'',price:'',quantity:'',sum:''},
+                                   item2:{code:'',name:'',price:'',quantity:'',sum:''}
                                   });
             };
             const focusForm = (index) => {
@@ -20,6 +44,7 @@ window.addEventListener('load',(event) => {
                 const targetForm = forms.value[index];
                 targetForm.isActive = true;
                 form.value = targetForm;
+                calcSum();
             };
             const deleteForm = (index) => {
                 if(!confirm("本当に削除しますか?(元に戻せません)")) return;
@@ -32,19 +57,36 @@ window.addEventListener('load',(event) => {
                 // [TODO] API を叩いて保存する
                 console.log(saveForms);
             };
+            const loadData =  (newData) => {
+                forms.value = newData;
+                forms.value.forEach((f,index) => {
+                    if (index == 0){
+                        f.isActive = true;
+                    } else {
+                        f.isActive = false;
+                    }
+                });
+                form.value = forms.value.find((item) => item.isActive);
+                calcSum();
+            };
             const test = () => {
-                form.value.code = '201';
-                form.value.name = 'やまだ';
-                form.value.age = '89';
-                form.value.item0.code = '111';
-                form.value.item0.name = '家電1';
-                form.value.item0.price = '1000';
-                form.value.item1.code = '222';
-                form.value.item1.name = '家電2';
-                form.value.item1.price = '2000';
-                form.value.item2.code = '333';
-                form.value.item2.name = '家電3';
-                form.value.item2.price = '3000';
+                const newData = [{code:'101',name:'三辻',age:'47',
+                                 item0:{code:'101',name:'PC',price:'50000',quantity:'1',sum:'50000'},
+                                 item1:{code:'102',name:'モニタ',price:'10000',quantity:'2',sum:'20000'},
+                                 item2:{code:'',name:'',price:'',quantity:'',sum:''}
+                                },
+                                {code:'102',name:'鈴木',age:'35',
+                                 item0:{code:'',name:'',price:'',quantity:'',sum:''},
+                                 item1:{code:'',name:'',price:'',quantity:'',sum:''},
+                                 item2:{code:'',name:'',price:'',quantity:'',sum:''}
+                                },
+                                {code:'103',name:'森田',age:'25',
+                                 item0:{code:'101',name:'PC',price:'50000',quantity:'1',sum:'50000'},
+                                 item1:{code:'102',name:'モニタ',price:'10000',quantity:'2',sum:'10000'},
+                                 item2:{code:'103',name:'タブレット',price:'30000',quantity:'3',sum:'30000'}
+                                }];
+
+                loadData(newData);
             };
             const openSubwin = (index) => {
                 window.open(`subwin.html?callback=setItem&index=${index}`, "URL", "width=280,height=320,toolbar=0");
@@ -55,42 +97,20 @@ window.addEventListener('load',(event) => {
                 item.code = code;
                 item.name = name;
                 item.price = price;
+                calcSum();
             };
             onMounted (async () => {
                 // [TODO] API を叩いてとってくる
-//                const initForms = [{code:'101',name:'三辻',age:'47',
-//                                    item0:{code:'101',name:'PC',price:'50000'},
-//                                    item1:{code:'102',name:'モニタ',price:'10000'},
-//                                    item2:{code:'',name:'',price:''}
-//                                   },
-//                                   {code:'102',name:'鈴木',age:'35',
-//                                    item0:{code:'',name:'',price:''},
-//                                    item1:{code:'',name:'',price:''},
-//                                    item2:{code:'',name:'',price:''}
-//                                   },
-//                                   {code:'103',name:'森田',age:'25',
-//                                    item0:{code:'101',name:'PC',price:'50000'},
-//                                    item1:{code:'102',name:'モニタ',price:'10000'},
-//                                    item2:{code:'103',name:'タブレット',price:'30000'}
-//                                   }];
                 const initForms = [{code:'',name:'',age:'',
-                                    item0:{code:'',name:'',price:''},
-                                    item1:{code:'',name:'',price:''},
-                                    item2:{code:'',name:'',price:''}
+                                    item0:{code:'',name:'',price:'',quantity:'',sum:''},
+                                    item1:{code:'',name:'',price:'',quantity:'',sum:''},
+                                    item2:{code:'',name:'',price:'',quantity:'',sum:''}
                                    }];
-                forms.value = initForms;
-                forms.value.forEach((f,index) => {
-                    if (index == 0){
-                        f.isActive = true;
-                    } else {
-                        f.isActive = false;
-                    }
-                });
-                form.value = forms.value.find((item) => item.isActive);
+                loadData(initForms);
 
             });
             window.setItem = setItem; // サブウィンドウから呼び出すためにグローバルに登録
-            return { forms,form, appendForm, focusForm, deleteForm, save, test, openSubwin};
+            return { forms,form, appendForm, focusForm, deleteForm, save, test, openSubwin, calcSum};
         }
     });
     app.config.compilerOptions.delimiters = ['(#', '#)'];
